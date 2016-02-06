885d77ca97e1c80f11f4b37bd91406a6dbb7d115
Jeonghoon Kim <jeonghoon.kim@stonybrook.edu>
2016-02-06 15:29:45 -0500

    Appended instruction in to the list

--- ssm.py
+++ ssm.py
@@ -1,1 +1,21 @@
-// SSM Interpreter
+import re
+import sys
+
+def scanner():
+
+	lines = sys.stdin.readlines()
+	newline = []
+	for i in range(len(lines)):
+		words = lines[i].split()
+		for word in words:
+			newline.append(word)
+		#lines[i] = lines[i].lstrip(' ')
+		#lines[i] = lines[i].replace('\n','')
+	return newline
+
+def main():
+	lines = scanner()
+	
+	print lines
+if __name__ == '__main__':
+    main()
