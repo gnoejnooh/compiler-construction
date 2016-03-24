c97f8fa74fc208666d02ad2576c1060ee181d226
Jeonghoon Kim <jeonghoon.kim@stonybrook.edu>
2016-03-23 21:58:43 -0400

    more tree

--- decafparser.py
+++ decafparser.py
@@ -98,39 +98,34 @@ def p_method_decl_nonvoid(p):
 def p_constructor_decl(p):
     'constructor_decl : mod ID LPAREN param_list_opt RPAREN block'
     p[0] = CONSTRUCTOR_DECL(p[1], p[4], p[6])
-    #pass
 
 def p_mod(p):
     'mod : visibility_mod storage_mod'
     p[0] = MOD(p[1], p[2])
-    #pass
 
 def p_visibility_mod_pub(p):
     'visibility_mod : PUBLIC'
     p[0] = VISIBILITY_MOD(p[1])
-    #pass
+
 def p_visibility_mod_priv(p):
     'visibility_mod : PRIVATE'
     p[0] = VISIBILITY_MOD(p[1])
-    #pass
+
 def p_visibility_mod_empty(p):
     'visibility_mod : '
     p[0] = VISIBILITY_MOD(None)
-    #pass
 
 def p_storage_mod_static(p):
     'storage_mod : STATIC'
     p[0] = STORAGE_MOD(p[1])
-    #pass
+
 def p_storage_mod_empty(p):
     'storage_mod : '
     p[0] = STORAGE_MOD(None)
-    #pass
 
 def p_var_decl(p):
     'var_decl : type var_list SEMICOLON'
     p[0] = VAR_DECL(p[1], p[2])
-    #pass
 
 def p_type_int(p):
     'type :  INT'
@@ -147,28 +142,26 @@ def p_type_float(p):
 def p_type_id(p):
     'type :  ID'
     p[0] = TYPE(p[1])
-    #pass
 
 def p_var_list_plus(p):
     'var_list : var_list COMMA var'
     p[0] = VAR_LIST(p[1], p[3])
-    #pass
+
 def p_var_list_single(p):
     'var_list : var'
     p[0] = VAR_LIST(None, p[1])
-    #pass
 
 def p_var_id(p):
     'var : ID'
     p[0] = VAR(p[1])
-    #pass
+
 def p_var_array(p):
     'var : var LBRACKET RBRACKET'
     p[0] = p[1]
-    #pass
 
 def p_param_list_opt(p):
     'param_list_opt : param_list'
+    p[0] = PARAM_LIST_OPT(p[1])
     pass
 def p_param_list_empty(p):
     'param_list_opt : '
@@ -176,21 +169,22 @@ def p_param_list_empty(p):
 
 def p_param_list(p):
     'param_list : param_list COMMA param'
-    pass
+    p[0] = PARAM_LIST(p[1], p[3])
+
 def p_param_list_single(p):
     'param_list : param'
-    pass
+    p[0] = PARAM_LIST(None, p[1])
 
 def p_param(p):
     'param : type ID'
-    pass
+    p[0] = PARAM(p[1], p[2])
 
 # Statements
 
 def p_block(p):
     'block : LBRACE stmt_list RBRACE'
     p[0] = BLOCK(p[2])
-    #pass
+
 def p_block_error(p):
     'block : LBRACE stmt_list error RBRACE'
     # error within a block; skip to enclosing block
@@ -199,42 +193,51 @@ def p_block_error(p):
 def p_stmt_list_empty(p):
     'stmt_list : '
     p[0] = STMT_LIST(None, None)
-    #pass
+
 def p_stmt_list(p):
     'stmt_list : stmt_list stmt'
     p[0] = STMT_LIST(p[1], p[2])
-    #pass
-
 
 def p_stmt_if(p):
     '''stmt : IF LPAREN expr RPAREN stmt ELSE stmt
           | IF LPAREN expr RPAREN stmt'''
-    pass
+    if(len(p) == 8):
+        p[0] = STMT_IF(p[1], p[3], p[5], p[6], p[7])
+    else:
+        p[0] = STMT_IF(p[1], p[3], p[5], None, None)
+
 def p_stmt_while(p):
     'stmt : WHILE LPAREN expr RPAREN stmt'
-    pass
+    p[0] = STMT_WHILE(p[1], p[3], p[5])
+
 def p_stmt_for(p):
     'stmt : FOR LPAREN stmt_expr_opt SEMICOLON expr_opt SEMICOLON stmt_expr_opt RPAREN stmt'
-    pass
+    p[0] = STMT_FOR(p[1], p[3], p[5], p[7], p[9])
+
 def p_stmt_return(p):
     'stmt : RETURN expr_opt SEMICOLON'
-    pass
+    p[0] = STMT_RETURN(p[1], p[2])
+
 def p_stmt_stmt_expr(p):
     'stmt : stmt_expr SEMICOLON'
     p[0] = STMT_STMT_EXPR(p[1])
     #pass
 def p_stmt_break(p):
     'stmt : BREAK SEMICOLON'
-    pass
+    p[0] = STMT_BREAK(p[1])
+
 def p_stmt_continue(p):
     'stmt : CONTINUE SEMICOLON'
-    pass
+    p[0] = STMT_CONTINUE(p[1])
+
 def p_stmt_block(p):
     'stmt : block'
-    pass
+    p[0] = STMT_BLOCK(p[1])
+
 def p_stmt_var_decl(p):
     'stmt : var_decl'
-    pass
+    p[0] = STMT_VAR_DECL(p[1])
+
 def p_stmt_error(p):
     'stmt : error SEMICOLON'
     print("Invalid statement near line {}".format(p.lineno(1)))
@@ -244,83 +247,91 @@ def p_stmt_error(p):
 def p_literal_int_const(p):
     'literal : INT_CONST'
     p[0] = LITERAL_INT_CONST(p[1])
-    #pass
+
 def p_literal_float_const(p):
     'literal : FLOAT_CONST'
-    pass
+    p[0] = LITERAL_FLOAT_CONST(p[1])
+
 def p_literal_string_const(p):
     'literal : STRING_CONST'
-    pass
+    p[0] = LITERAL_STRING_CONST(p[1])
+
 def p_literal_null(p):
     'literal : NULL'
-    pass
+    p[0] = LITERAL_NULL(p[1])
+
 def p_literal_true(p):
     'literal : TRUE'
-    pass
+    p[0] = LITERAL_TRUE(p[1])
+
 def p_literal_false(p):
     'literal : FALSE'
-    pass
+    p[0] = LITERAL_FALSE(p[1])
 
 def p_primary_literal(p):
     'primary : literal'
-    p[0] = PRIMARY(p[1])
-    #pass
+    p[0] = PRIMARY_LITERAL(p[1])
+
 def p_primary_this(p):
     'primary : THIS'
-    p[0] = PRIMARY(p[1])
-    #pass
+    p[0] = PRIMARY_THIS(p[1])
+
 def p_primary_super(p):
     'primary : SUPER'
-    pass
+    p[0] = PRIMARY_SUPER(p[1])
+
 def p_primary_paren(p):
     'primary : LPAREN expr RPAREN'
-    pass
+    p[0] = PRIMARY_LITERAL(p[1])
+
 def p_primary_newobj(p):
     'primary : NEW ID LPAREN args_opt RPAREN'
-    pass
+    p[0] = PRIMARY_NEWOBJ(p[2], p[4])
+
 def p_primary_lhs(p):
     'primary : lhs'
-    pass
+    p[0] = PRIMARY_LHS(p[1])
+
 def p_primary_method_invocation(p):
     'primary : method_invocation'
-    pass
+    p[0] = PRIMARY_METHOD_INVOCATION(p[1])
 
 def p_args_opt_nonempty(p):
     'args_opt : arg_plus'
-    pass
+    p[0] = ARGS_OPT(p[1])
+
 def p_args_opt_empty(p):
     'args_opt : '
-    pass
+    p[0] = ARGS_OPT(None)
 
 def p_args_plus(p):
     'arg_plus : arg_plus COMMA expr'
-    pass
+    p[0] = ARGS_PLUS(p[1], p[3])
+
 def p_args_single(p):
     'arg_plus : expr'
-    pass
+    p[0] = ARGS_PLUS(None, p[1])
 
 def p_lhs(p):
     '''lhs : field_access
            | array_access'''
     p[0] = LHS(p[1])
-    #pass
 
 def p_field_access_dot(p):
     'field_access : primary DOT ID'
     p[0] = FIELD_ACCESS(p[1], p[3])
-    #pass
+
 def p_field_access_id(p):
     'field_access : ID'
     p[0] = FIELD_ACCESS(None, p[1])
-    #pass
 
 def p_array_access(p):
     'array_access : primary LBRACKET expr RBRACKET'
-    pass
+    p[0] = ARRAY_ACCESS(p[1], p[3])
 
 def p_method_invocation(p):
     'method_invocation : field_access LPAREN args_opt RPAREN'
-    pass
+    p[0] = METHOD_INVOCATION(p[1], p[3])
 
 def p_expr_basic(p):
     '''expr : primary
